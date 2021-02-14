9214b5fb767a21345564bf42b26bfb37b335d73a
Blake Chartrand <chartrandblake@hotmail.com>
2021-02-13 23:29:23 -0800

    Reverting to 14f77AC49f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   def index
    @item = Item.order(:name)
   end
-
+ 
   def show
     @item = Item.find(params[:id])
   end
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
       render 'edit'
     end
   end
-
-  
+ 
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
@@ -46,7 +45,7 @@ class ItemsController < ApplicationController
  
   private
     def item_params
-      params.require(:item).permit(:name, :description, :tags)
+      params.require(:item).permit(:name, :description)
     end
 
     http_basic_authenticate_with name: "blake", password: "secret", except: [:index, :show]
